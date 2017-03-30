38974b8dd20f33019fe1661d58a8493fc8906f52
Kirk <KirkJohnson21@yahoo.com>
2017-03-30 01:55:30 +0000

    pushing to Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
+  resources :articles
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root 'pages#home'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  
+  get 'about' => 'pages#about'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
