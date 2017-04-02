af1833749fbdbad34bc9773e030591e6fa45ee7c
Kirk <KirkJohnson21@yahoo.com>
2017-04-02 09:10:49 +0000

    Complete authentication system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:edit, :update, :show]
+    before_action :require_same_user, only: [:edit, :update]
     def index
        @users = User.paginate(page: params[:page], per_page: 5)
     end
@@ -17,11 +19,10 @@ class UsersController < ApplicationController
     end
     
     def edit
-        @user = User.find(params[:id])        
+               
     end
     
     def update
-        @user = User.find(params[:id])
         
         if @user.update(user_params)
             flash[:success] = "Your account was updated successfully"
@@ -32,11 +33,24 @@ class UsersController < ApplicationController
     end
     
     def show
-       @user = User.find(params[:id])
        @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
     private
     def user_params
         params.require(:user).permit(:username, :email,:password)
+    end
+    
+    def set_user
+        @user = User.find(params[:id]) 
+    end
+    
+    def require_same_user
+        if logged_in? && current_user != @user
+            flash[:danger] = "You can only your own account."
+            redirect_to root_path
+        elsif !logged_in?
+            flash[:danger] = "You don't have access to this functionality"
+            redirect_to root_path
+        end
     end
 end
